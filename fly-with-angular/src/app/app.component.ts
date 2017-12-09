1cc6e7b54b55e96be50f03e9da0b81a88978f02b
sagar dixit <sagar.dixit@ninestack.com>
2017-12-09 13:34:50 -0800

    first commit- project setup

--- fly-with-angular/src/app/app.component.ts
+++ fly-with-angular/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  
+  TripName="Trip-name";
+  TripTime="Trip-time";
+  TripDesc="Trip-desc";
+  TripPrice="Trip-price";
+
 }
